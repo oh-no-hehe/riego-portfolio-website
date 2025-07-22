f48f06b9edd78cb0cedeef5cabbc8aa983367b9a
Hexter <riegoraven839@gmail.com>
2025-07-22 23:39:17 +0800

    update website

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('navbar');
+
+menuToggleBtn.addEventListener('click', () => {
+  navbar.a.classList.toggle('show');
+});
